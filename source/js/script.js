b0a533cc49e3584a5fefe94e62e6d9752846cf91
kirillsvi <kirillsvitov405@gmail.com>
2023-01-19 18:55:46 +0400

    homeTask#1

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+let userAge = 64;
+let userEnter = "hello";
+let planeIsFly = true;
+let bigInt = 18947298752438954385n;
+let zeroValue = null; 
+let j;
+let whateverObject = {};
+let whateverSymbol = Symbol("WhateverSymbol");
+
+console.log(Number(userAge), String(userAge), Boolean(userAge));
+console.log(Number(userEnter), String(userEnter), Boolean(userEnter));
+console.log(Number(planeIsFly), String(planeIsFly), Boolean(planeIsFly));
+console.log(Number(bigInt), String(bigInt), Boolean(bigInt));
+console.log(Number(zeroValue), String(zeroValue), Boolean(zeroValue));
+console.log(Number(j), String(j), Boolean(j));
+console.log(Number(whateverObject), String(whateverObject), Boolean(whateverObject));
+console.log(String(whateverSymbol), Boolean(whateverSymbol));
+    
+
