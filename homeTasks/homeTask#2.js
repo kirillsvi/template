a4682acf7a69bd845cea4df26b48a726d0d35268
kirillsvi <kirillsvitov405@gmail.com>
2023-01-25 14:10:04 +0400

    homeTask#2

---
+++ homeTasks/homeTask#2.js
@@ -0,0 +1,54 @@
+
+// Task#1
+// let userNumber = prompt();
+// if (isNaN(userNumber)) {
+//   console.log("Нужно писать число"); 
+// }
+// for(let i = 1; i < userNumber; i++) {
+//   if( i%4===0) continue;
+//   console.log(i);
+  
+// }
+
+
+// Task#2
+// let userNumber = prompt();
+//   if (isNaN(userNumber)) {
+//     console.log("Нужно писать число"); 
+//   }
+// let userNumberFactorial = 1;
+// while (userNumber > 1){
+//     userNumberFactorial = userNumberFactorial * userNumber;
+//     userNumber = userNumber - 1;
+// }
+// console.log (userNumberFactorial);
+
+// если введешь строку, после "нужно писать число" однерку напишет. Не знаю как поправить
+
+
+// Task#3
+// let userNumber = prompt("Введите число");
+// let userNumberDegree = prompt("Введите его степень");
+
+// if (isNaN(userNumber&&userNumberDegree)) {
+//     console.log("Нужно писать число"); 
+// }
+// for( ; userNumberDegree > 1; ) {
+//   userNumber = userNumber * userNumber;
+//   userNumberDegree = userNumberDegree - 1;
+// }
+// console.log(userNumber);
+
+// тут так же как и во втором
+
+
+// Task#5
+// let rand = Math.floor(1 + Math.random() * 10);
+// let i = 1
+// while (i != rand) {
+//   let i = prompt();
+//   if(i == rand) {
+//     console.log("Победа");
+//     break;
+//   }
+// }
